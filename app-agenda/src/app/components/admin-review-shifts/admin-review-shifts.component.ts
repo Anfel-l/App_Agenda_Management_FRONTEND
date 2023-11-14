131abec5ec2e05c27c070d0ba1de64a9a210ee00
Anfel-l <anfellr11@gmail.com>
2023-11-14 02:59:26 -0500

    feat(ico): favicon updated

--- app-agenda/src/app/components/admin-review-shifts/admin-review-shifts.component.ts
+++ app-agenda/src/app/components/admin-review-shifts/admin-review-shifts.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
 import { DoctorService } from './doctor.service';
 import { DoctorDetail } from './models/doctor.detail.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-admin-review-shifts',
@@ -21,6 +22,6 @@ export class AdminReviewShiftsComponent implements OnInit {
       )
       .subscribe((details) => {
         this.doctorDetails = details;
-      });
+      }, (error) => Swal.fire('Error', "No se han cargado los turnos de todos los médicos", 'error'));
   }
 }
